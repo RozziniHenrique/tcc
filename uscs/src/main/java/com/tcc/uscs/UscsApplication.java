cfec44492b3678659d59c65f109b87ad9e40b3e3
RozziniHenrique <hrozzini@gmail.com>
2026-03-13 07:42:21 -0300

    Inicio do TCC

---
+++ uscs/src/main/java/com/tcc/uscs/UscsApplication.java
@@ -0,0 +1,13 @@
+package com.tcc.uscs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UscsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UscsApplication.class, args);
+	}
+
+}
